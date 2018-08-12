0b5bede4a250a1d6e7f461d0290add03f6b2b9cf
Juan Louw <jplouw.consulting@gmail.com>
2018-08-12 12:45:28 +0100

    Create LoadDataToSQL.ps1

---
+++ LoadDataToSQL.ps1
@@ -0,0 +1,37 @@
+get-Childitem -Path "C:\FMBPublic\Downloads\Upload\dm_spl_release_human_rx_part1\prescription\*" -Include *.xml -Recurse
+$XMLFiles
+
+$XMLFiles = get-childitem "C:\FMBPublic\Downloads\Upload\dm_spl_release_human_rx_part1\prescription\" -recurse | where {$_.extension -eq ".xml"} | % {
+      $_.FullName
+}
+
+
+Foreach ($XMLFile in $XMLFiles) {
+$XMLFileName = Split-Path $XMLFile -leaf
+$XMLFilePath = $XMLFile -Replace $XMLFileName, ''
+#$XMLFilePath
+$XMLFileName = $XMLFileName -Replace '.xml',''
+#$XMLFileName
+#$XMLFile
+
+$SqlConnection = new-object System.Data.SqlClient.SqlConnection
+$SqlConnection.ConnectionString = "Data Source=SQL2016;Database=FMBPublic;Integrated Security=True;"
+$SqlCommand = New-Object System.Data.SqlClient.SqlCommand
+$SqlCommand.CommandText = "Exec [dbo].[spInsertXMLData] @XMLDataID, @PhotoLocation, @FileLocation"
+$SqlCommand
+# Add parameters to pass values to the stored procedure
+$SqlCommand.Parameters.AddWithValue("@XMLDataID", $XMLFileName) | Out-Null
+$SqlCommand.Parameters.AddWithValue("@PhotoLocation", $XMLFilePath) | Out-Null
+$SqlCommand.Parameters.AddWithValue("@FileLocation", $XMLFile) | Out-Null
+$SQLCommand.Connection = $SqlConnection
+$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+$SqlAdapter.SelectCommand = $SqlCommand
+$DataSet = New-Object System.Data.DataSet
+$SqlAdapter.Fill($DataSet)
+$SqlConnection.Close()
+$DataSet.Tables[0]
+
+Clear-Variable -Name sqlCommand
+$SqlCommand
+pause
+}
