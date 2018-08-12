c2001b9a9dab55629ea5e5b02249f1eb0b5785ab
Juan Louw <jplouw.consulting@gmail.com>
2018-08-12 18:56:20 +0100

    Update Initial Load.ps1

--- Initial Load.ps1
+++ Initial Load.ps1
@@ -1,3 +1,31 @@
+ $output = "C:\FMBPublic\Downloads\dm_spl_release_human_rx_part3.zip"
+$targetFolder = "C:\FMBPublic\Downloads\Upload\dm_spl_release_human_rx_part3"
+[System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
+[System.IO.Compression.ZipFile]::ExtractToDirectory($output, $targetFolder)
+
+$ZipFilesPath = "$targetFolder\prescription"
+$UnzipPath = "$targetFolder\prescription\"
+$Shell = New-Object -com Shell.Application
+$Location = $Shell.NameSpace($UnzipPath)
+$ZipFiles = Get-Childitem $ZipFilesPath -Name -Recurse -Include *.ZIP
+$progress = 1
+foreach ($ZipFile in $ZipFiles) {
+    #Write-Progress -Activity "Unzipping to $($UnzipPath/$ZipFile)" -PercentComplete (($progress / ($ZipFiles.Count + 1)) * 100) -CurrentOperation $ZipFile.FullName -Status "File $($Progress) of $($ZipFiles.Count)"
+    $ZipName = $ZipFile -Replace ".zip",""
+    $ZipName
+    $UnZipFolder = $UnzipPath+$ZipName
+    $ZipFolder = $UnzipPath+$ZipFile
+    write-output  $ZipFolder 
+    write-output  $UnZipFolder 
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($ZipFolder,$UnZipFolder)
+    $progress++
+    Remove-Item -path $ZipFolder
+}
+
+$Acl = Get-Acl $targetFolder
+Set-Acl $targetFolder $Acl 
+ 
+ 
  $output = "C:\FMBPublic\Downloads\dm_spl_release_human_rx_part1.zip"
 
 
